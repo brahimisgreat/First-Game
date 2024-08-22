617e5f414d941e602adc27ad80f0102f09308964
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-08-22 19:10:17 -0400

    javascrpit

--- main.js
+++ main.js
@@ -1,2 +1,38 @@
 import './style.css'
+import Phaser, { Game, Physics } from 'phaser'
 
+const sizes={
+  width:500,
+  height:500
+}
+const speedDown=300
+
+class GameScene extends Phaser.Scene{
+  constructor(){
+    super('scene-game')
+  }
+
+  preload(){}
+  create(){}
+  update(){}
+
+    
+}
+
+const config = {
+  type:Phaser.WEBGL,
+  width: sizes.width,
+  height: sizes.height,
+  canvas:gameCanvas,
+  Physics:{
+    default:'arcade',
+    arcade:{
+      gravity:{y:speedDown},
+      debug:true
+    }
+}
+}
+
+scene:[GameScene]
+
+const game = new Phaser.Game(config)
